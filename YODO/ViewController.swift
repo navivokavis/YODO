eb3e9c5b9d8d706b82b3d72aebbfe3e1d30f8247
navivokavis <navivokavis@gmail.com>
2022-09-09 16:13:30 +0300

    Initial Commit

---
+++ YODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YODO
+//
+//  Created by s x on 9.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
